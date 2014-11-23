72543f7fccc754e6c44f786ccbd650a8d7fbb3e0
“jeffmccommas” <jeffmccommas@gmail.com>
2014-11-23 07:03:33 -0500

    ready to deploy

--- store/server.js
+++ store/server.js
@@ -1,16 +1,14 @@
-/**
- * Created by jmccommas on 11/22/14.
- */
-var express = require('express');
-    stylus = require('stylus');
-    logger = require('morgan');
-    bodyParser = require('body-parser');
+var express = require('express'),
+    stylus = require('stylus'),
+    logger = require('morgan'),
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
-function compile(str,path){
+function compile(str, path) {
     return stylus(str).set('filename', path);
 }
 
@@ -26,15 +24,42 @@ app.use(stylus.middleware(
 ));
 app.use(express.static(__dirname + '/public'));
 
+if(env === 'development') {
+ mongoose.connect('mongodb://localhost/productmanager');
+    } else {
+ mongoose.connect('mongodb://jmccommas:fullsail@ds059220-a0.mongolab.com:59220,ds059220-a1.mongolab.com:59220/productmanager');
+}
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function callback() {
+    console.log('product mananger db opened');
+});
+
+var messageSchema = mongoose.Schema({message: String});
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage= new Message({message: mongoMessage});
+console.log(mongoMessage.message);
+Message.findOne().exec(function(err, messageDoc) {
+    mongoMessage = messageDoc.message;
+});
+
 app.get('/partials/:partialPath', function(req, res) {
     res.render('partials/' + req.params.partialPath);
 });
 
-
-app.get('*', function (req, res) {
-   res.render('index');
+app.get('*', function(req, res) {
+    res.render('index', {
+        mongoMessage: mongoMessage
+    });
 });
 
-var port = 3030;
+mongoMessage.save(function(err, doc) {
+    Message.findOne().exec(function(err, messageDoc) {
+        mongoMessage = messageDoc.message;
+    });
+});
+
+var port = process.env.PORT || 3030;
 app.listen(port);
-console.log("listening on port" + port + "....");
+console.log('Listening on port ' + port + '...');
